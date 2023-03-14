5d6b0324210caeb1d8cfd43af6c2609e1780a6f6
Hapari Paya <hranghlukoko@gmail.com>
2023-03-14 11:24:07 +0530

    used fetch to use json file

--- script.js
+++ script.js
@@ -1,169 +1,63 @@
-const jobList = [
-  {
-    id: 1,
-    company: "Photosnap",
-    logo: "./images/photosnap.svg",
-    new: true,
-    featured: true,
-    position: "Senior Frontend Developer",
-    role: "Frontend",
-    level: "Senior",
-    postedAt: "1d ago",
-    contract: "Full Time",
-    location: "USA Only",
-    languages: ["HTML", "CSS", "JavaScript"],
-    tools: [],
-  },
-  {
-    id: 2,
-    company: "Manage",
-    logo: "./images/manage.svg",
-    new: true,
-    featured: true,
-    position: "Fullstack Developer",
-    role: "Fullstack",
-    level: "Midweight",
-    postedAt: "1d ago",
-    contract: "Part Time",
-    location: "Remote",
-    languages: ["Python"],
-    tools: ["React"],
-  },
-  {
-    id: 3,
-    company: "Account",
-    logo: "./images/account.svg",
-    new: true,
-    featured: false,
-    position: "Junior Frontend Developer",
-    role: "Frontend",
-    level: "Junior",
-    postedAt: "2d ago",
-    contract: "Part Time",
-    location: "USA Only",
-    languages: ["JavaScript"],
-    tools: ["React", "Sass"],
-  },
-  {
-    id: 4,
-    company: "MyHome",
-    logo: "./images/myhome.svg",
-    new: false,
-    featured: false,
-    position: "Junior Frontend Developer",
-    role: "Frontend",
-    level: "Junior",
-    postedAt: "5d ago",
-    contract: "Full Time",
-    location: "USA Only",
-    languages: ["CSS", "JavaScript"],
-    tools: [],
-  },
-  {
-    id: 5,
-    company: "Loop Studios",
-    logo: "./images/loop-studios.svg",
-    new: false,
-    featured: false,
-    position: "Software Engineer",
-    role: "Fullstack",
-    level: "Midweight",
-    postedAt: "1w ago",
-    contract: "Full Time",
-    location: "Worldwide",
-    languages: ["JavaScript"],
-    tools: ["Ruby", "Sass"],
-  },
-  {
-    id: 6,
-    company: "FaceIt",
-    logo: "./images/faceit.svg",
-    new: false,
-    featured: false,
-    position: "Junior Backend Developer",
-    role: "Backend",
-    level: "Junior",
-    postedAt: "2w ago",
-    contract: "Full Time",
-    location: "UK Only",
-    languages: ["Ruby"],
-    tools: ["RoR"],
-  },
-  {
-    id: 7,
-    company: "Shortly",
-    logo: "./images/shortly.svg",
-    new: false,
-    featured: false,
-    position: "Junior Developer",
-    role: "Frontend",
-    level: "Junior",
-    postedAt: "2w ago",
-    contract: "Full Time",
-    location: "Worldwide",
-    languages: ["HTML", "JavaScript"],
-    tools: ["Sass"],
-  },
-  {
-    id: 8,
-    company: "Insure",
-    logo: "./images/insure.svg",
-    new: false,
-    featured: false,
-    position: "Junior Frontend Developer",
-    role: "Frontend",
-    level: "Junior",
-    postedAt: "2w ago",
-    contract: "Full Time",
-    location: "USA Only",
-    languages: ["JavaScript"],
-    tools: ["Vue", "Sass"],
-  },
-  {
-    id: 9,
-    company: "Eyecam Co.",
-    logo: "./images/eyecam-co.svg",
-    new: false,
-    featured: false,
-    position: "Full Stack Engineer",
-    role: "Fullstack",
-    level: "Midweight",
-    postedAt: "3w ago",
-    contract: "Full Time",
-    location: "Worldwide",
-    languages: ["JavaScript", "Python"],
-    tools: ["Django"],
-  },
-  {
-    id: 10,
-    company: "The Air Filter Company",
-    logo: "./images/the-air-filter-company.svg",
-    new: false,
-    featured: false,
-    position: "Front-end Dev",
-    role: "Frontend",
-    level: "Junior",
-    postedAt: "1mo ago",
-    contract: "Part Time",
-    location: "Worldwide",
-    languages: ["JavaScript"],
-    tools: ["React", "Sass"],
-  },
-];
-/* packing all the tags to be used found in various properties of an
-object into a single array using spread operato,
-because that makes my life easier! */
-jobList.forEach((job) => {
-  // job.tags = job.tools.concat(job.languages, job.role, job.level);
-  job.tags = [job.role, job.level, ...job.languages, ...job.tools]
-});
-
-const card_container = document.querySelector(".job_cards");
+import dataTamul from "./data.json" assert {type: 'json'};
+const jobs_container = document.querySelector(".job_cards");
 const filter_section = document.querySelector(".filter_section");
 const filter_tags = document.querySelector(".filter_tags");
+let jobList;
+
+fetch("data.json")
+  .then((res) => res.json())
+  .then((data) => {
+    data.forEach((job) => {
+      // job.tags = job.tools.concat(job.languages, job.role, job.level);
+      job.tags = [job.role, job.level, ...job.languages, ...job.tools];
+    });
+    jobList = data;
+    renderJobs(data);
+  });
+
+let tags = [];
+
+jobs_container.addEventListener("click", (e) => {
+  const clickedEl = e.target;
+  if (clickedEl.hasAttribute(`data-filter`)) {
+    if (tags.includes(clickedEl.dataset.filter)) return;
+    tags.push(clickedEl.dataset.filter);
+    const tag = document.createElement("div");
+    tag.classList.add("tag");
+    tag.id = `${clickedEl.dataset.filter}`;
+    tag.innerHTML = `
+    <span class="tag_text">${clickedEl.dataset.filter}</span>
+    <button class="tag_close_icon">
+      <img class="tag_close_icon" src="images/icon-remove.svg" />
+    </button>
+    `;
+    filter_tags.appendChild(tag);
+    filter_section.style.opacity = 1;
+    filter_section.style.pointerEvents = "auto";
+    filterJobs();
+  }
+});
+
+filter_section.addEventListener("click", (e) => {
+  const clicked = e.target;
+  if (clicked.id == "tags_clear_btn") {
+    filter_tags.innerHTML = "";
+    tags = [];
+    closeFilterBox();
+    renderJobs(jobList);
+  } else if (clicked.classList.contains("tag_close_icon")) {
+    const tag = clicked.closest("div");
+    tag.remove();
+    tags = tags.filter((tagStr) => tag.id != tagStr);
+    if (tags.length == 0) {
+      closeFilterBox();
+      renderJobs(jobList);
+    } else filterJobs();
+  }
+});
 
 function renderJobs(jobs) {
-  card_container.innerHTML = "";
+  jobs_container.innerHTML = "";
   jobs.forEach((jobData) => {
     const article = document.createElement("article");
     const filter = jobData.tags
@@ -192,33 +86,9 @@ function renderJobs(jobs) {
           ${filter}
         </section>
     `;
-    card_container.appendChild(article);
+    jobs_container.appendChild(article);
   });
 }
-renderJobs(jobList);
-
-let tags = [];
-card_container.addEventListener("click", (e) => {
-  const clickedEl = e.target;
-  if (clickedEl.hasAttribute(`data-filter`)) {
-    if (tags.includes(clickedEl.dataset.filter)) return;
-    tags.push(clickedEl.dataset.filter);
-    const tag = document.createElement("div");
-    tag.classList.add("tag");
-    tag.id = `${clickedEl.dataset.filter}`;
-    tag.innerHTML = `
-    <span class="tag_text">${clickedEl.dataset.filter}</span>
-    <button class="tag_close_icon">
-      <img class="tag_close_icon" src="images/icon-remove.svg" />
-    </button>
-    `;
-    filter_tags.appendChild(tag);
-    filter_section.style.opacity = 1;
-    filter_section.style.pointerEvents = "auto";
-    filterJobs();
-  }
-});
-
 function filterJobs() {
   // checking if all the array of strings which represent the tags selected are found in an object property, if so the object has all the required taggings so it will show up
   const newJobList = jobList.filter((job) =>
@@ -226,39 +96,26 @@ function filterJobs() {
   );
   renderJobs(newJobList);
 }
-
-filter_section.addEventListener("click", (e) => {
-  const clicked = e.target;
-  if (clicked.classList.contains('tag_close_icon')) {
-    const tag = clicked.closest("div");
-    tag.remove();
-    tags = tags.filter((tagStr) => tag.id != tagStr);
-    if (tags.length == 0) {
-      closeFilterBox();
-      renderJobs(jobList);
-    }
-    else filterJobs();
-  } 
-  else if (clicked.id == 'tags_clear_btn') {
-    filter_tags.innerHTML = "";
-    tags = [];
-    closeFilterBox();
-    renderJobs(jobList);
-  }
-});
-
 function closeFilterBox() {
   filter_section.style.opacity = 0;
   filter_section.style.pointerEvents = "none";
 }
 
 /*
-DONT READ THE CODE BELOW!
- Notice: Failed code and snippets, atleast it was able to filter an item based only of single tag :(
-const newData = data.filter(d => tags.some(t => d.tags.includes(t)));
-To see if all the strings in an array are found in an object
-for (let prop in jobData) {
-      if (jobData[prop] == tag) return jobData;
-        if (jobData[prop].includes(tag)) return jobData;
-      }
+Changes to make:
+-When any job object has the new and featured property true, add two span tag to indicate the highlights when you create the job card element from the object properties, for other objects that dont have dont create it when you add the element to DOM
+*/
+/*
+Behaviour(multi-filtering: shows the object when it has all the tags added)
+1. When document loads, render the jobs from the array.
+2. When clicking on job card tags, add that tags only once to the tags section.
+3. Now rerender the jobs based on the tag/tags added in the tags section. 
+4. When clicking on tags section:
+ - if clicked on clear button than clear everything and rerender the jobs
+ - if clicked on tag close button, close the tag and job associated with it and:
+   - if tags are all empty rerender the jobs
+   - else filter by rerendering it
+
+Continued Development: 
+1. Ability to choose to single filtering mode(shows object when it has any of the tags)
 */
